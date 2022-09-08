3ca51acc4cc8159424646f7fc75e1ed58e2c3717
Lguilherme3 <Gui_3@outlook.com>
2022-09-08 19:02:21 -0300

    classe-cliente

---
+++ classe-cliente.js
@@ -0,0 +1,20 @@
+class Cliente {
+    constructor(nome, email, cpf, saldo) {
+        this.nome = nome
+        this.email = email
+        this.cpf = cpf
+        this.saldo = saldo
+    }
+
+    depositar(valor) {
+        this.saldo += valor
+    }
+
+    exibirSaldo() {
+        console.log(this.saldo)
+    }
+}
+
+const Guilherme = new Cliente("Guilherme", "Gui_3@outlook.com", "40528733869", 100)
+
+console.log(Guilherme)
